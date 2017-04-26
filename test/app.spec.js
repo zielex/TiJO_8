a2555f6b08002faa3fc3a38f0142bb7247a99935
The Almighty <lord@gmail.com>
2017-04-26 11:13:56 +0200

    raport2 add it and except

--- test/app.spec.js
+++ test/app.spec.js
@@ -8,6 +8,9 @@ describe('reverseNumber', function ()
     it('should return reversed numbers', function () {
         expect(app.reverseNumber(123)).to.eql(321);
     });
+    it('should false if not number', function () {
+        expect(app.reverseNumber('asa', 1)).to.eql(false);
+    });
 });
 
 describe('returnOnlyLetter', function ()
@@ -15,12 +18,18 @@ describe('returnOnlyLetter', function ()
     it('should delete all except letters', function(){
         expect(app.returnOnlyLetter('akwari3um')).to.eql('akwarium');
     });
+    it('it should return false if numbers', function(){
+        expect(app.returnOnlyLetter(1231434234)).to.eql(false);
+    });
 });
 
 describe('isEmail', function ()
 {
     it('should return true if email is good or false if is not good', function(){
        expect(app.isEmail('waclaw@gmail.com')).to.eql(true);
+    });
+    it('should return false if is not good', function(){
+        expect(app.isEmail('@ws23.com.gamil')).to.eql(false);
     });
 });
 
