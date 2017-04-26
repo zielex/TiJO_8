fbe6183a00bba78c292f5e267b953eb34bde6f69
The Almighty <lord@gmail.com>
2017-04-26 10:48:24 +0200

    add it and except

--- test/app.spec.js
+++ test/app.spec.js
@@ -5,15 +5,22 @@ var app = require('../app/app');
 
 describe('reverseNumber', function ()
 {
-
+    it('should return reversed numbers', function () {
+        expect(app.reverseNumber(123)).to.eql(321);
+    });
 });
 
 describe('returnOnlyLetter', function ()
 {
-
+    it('should delete all except letters', function(){
+        expect(app.returnOnlyLetter('akwari3um')).to.eql('akwarium');
+    });
 });
 
 describe('isEmail', function ()
 {
-
+    it('should return true if email is good or false if is not good', function(){
+       expect(app.isEmail('waclaw@gmail.com')).to.eql(true);
+    });
 });
+
